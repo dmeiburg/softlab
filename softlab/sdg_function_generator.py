5d9e780a49590d09022abc7360d60d0ce2627521
Daniel Meiburg <daniel@bluethmann.de>
2020-03-09 01:31:31 +0100

    Add documentation to sdg_function_generator.py

--- softlab/sdg_function_generator.py
+++ softlab/sdg_function_generator.py
@@ -1,13 +1,38 @@
+"""Implements interface for Siglent SDG2000X function generators"""
+
 import pyvisa
 
 
 class SDGFunctionGenerator():
+    """Class with methods to controll Siglent SDG2000X function generators.
+
+    Attributes
+    ----------
+    device: pyvisa.resources.TCPIPInstrument
+    """
+
     def __init__(self, ip):
+        """Establishes connection to device
+
+        Parameters
+        ----------
+        ip : string
+            ip address of the function generator
+        """
         rm = pyvisa.ResourceManager()
         self.device = rm.open_resource("TCPIP::{}".format(ip))
 
-    def query(self, q):
-        return self.device.query(q)
+    def query(self, scpi):
+        """Sends SCPI command to device and returns response.
+
+        Parameters
+        ----------
+        scpi : string
+            SCPI command
+        """
+        return self.device.query(scpi)
 
     def idn(self):
+        """Gets device identification.
+        """
         return self.query("*IDN?")
